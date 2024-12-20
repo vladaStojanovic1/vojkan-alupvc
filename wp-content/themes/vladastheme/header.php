54328a3ec83a4ce4592bee4486a21cca8fbbf87a
vladasto <1vladastojanovic1@gmail.com>
2024-12-20 18:10:36 +0100

    Remove if else statement from favicon in header.php

--- wp-content/themes/vladastheme/header.php
+++ wp-content/themes/vladastheme/header.php
@@ -33,15 +33,14 @@ global $globalSite;
     <?php wp_head(); ?>
 
     <link rel="profile" href="http://gmpg.org/xfn/11">
-    <?php if(has_site_icon()): ?>
-        <link rel="icon" href="<?php echo get_site_icon_url(); ?>" type="image/x-icon" />
-    <?php else: ?>
-        <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri() . '/src/images/favicon/favicon-96x96.png'; ?>" sizes="96x96" />
-        <link rel="icon" type="image/svg+xml" href="<?php echo get_template_directory_uri() . '/src/images/favicon/favicon.svg'; ?>" />
-        <link rel="shortcut icon" href="<?php echo get_template_directory_uri() . '/src/images/favicon/favicon.ico'; ?>" />
-        <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri() . '/src/images/favicon/apple-touch-icon.png'; ?>" />
-        <link rel="manifest" href="<?php echo get_template_directory_uri() . '/src/images/favicon/site.webmanifest'; ?>" />
-    <?php endif; ?>
+
+    <!--Favicon-->
+    <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri() . '/src/images/favicon/favicon-96x96.png'; ?>" sizes="96x96" />
+    <link rel="icon" type="image/svg+xml" href="<?php echo get_template_directory_uri() . '/src/images/favicon/favicon.svg'; ?>" />
+    <link rel="shortcut icon" href="<?php echo get_template_directory_uri() . '/src/images/favicon/favicon.ico'; ?>" />
+    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri() . '/src/images/favicon/apple-touch-icon.png'; ?>" />
+    <meta name="apple-mobile-web-app-title" content="Vojkan ALU & PVC" />
+    <link rel="manifest" href="<?php echo get_template_directory_uri() . '/src/images/favicon/site.webmanifest'; ?>" />
 </head>
 
 <body <?php body_class(); ?>>
